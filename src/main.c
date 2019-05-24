df83f12f50930ccdfd2faeaf345eee78a0dd0ae2
Pspaulojr <paulossergio.pjr@gmail.com>
2019-05-24 12:25:54 -0300

    tirei printf desnecessarios, deixei Makefile na versao padrao

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 
 
 #define NMAX         64
-#define N_PROCESSOS  4
+#define N_THREADS    4
 
 unsigned int verifica_primo (unsigned int N);
 void* funcao_thread(void *arg);
@@ -16,25 +16,18 @@ unsigned int vetor_numeros[NMAX];
 int qntd_numeros = 0;
 int abrir_threads = 0;
 int qntd_primos = 0;
-int primos[64];
 
 int main () {
 
-  int thread_args[N_PROCESSOS];
+  int thread_args[N_THREADS];
 
-  for(int b = 0; b < NMAX; b++) {   /*forma de 'zerar' meu vetor*/
+  for(int b = 0; b < NMAX; b++) {   /*forma de 'zerar' meu vetor que tem os numeros a serem analisados*/
     vetor_numeros[b] = 0;
   }
-
-  for(int j; j < NMAX; j++ ) {  /*'zero' meu vetor cujo conteudo de cada indice eh 1 ou 0 (primo ou nao) */
-    primos[j] = '\0'; 
-  }
   
-  while(scanf("%u ", &vetor_numeros[qntd_numeros]) != EOF ){
+  while(scanf("%u ", &vetor_numeros[qntd_numeros]) != EOF ){  /*Conto quantos numeros que precisaram ser analisados*/
     qntd_numeros++;
   }
-
-  printf("Qntd_numeros:\t%d\n", qntd_numeros);
 
   pthread_t threads[4];
   
@@ -45,24 +38,20 @@ int main () {
     abrir_threads = qntd_numeros;
   }
   
-  
+/* Inicializo minhas threads */
+
   for (int i = 0; i < (abrir_threads) ; i++) {
     thread_args[i] = i;
     pthread_create(&(threads[i]), NULL, funcao_thread, &(thread_args[i]) );
   }
 
+/*Espero minha threads finalizarem */
 
   for (int i = 0; i < abrir_threads; i++) {
     pthread_join(threads[i], NULL);
   }
 
-  /* for(int k = 0 ; k  < qntd_numeros ; k += N_PROCESSOS) {
-    pthread_mutex_lock(&trava);
-    qntd_primos += verifica_primo( vetor_numeros[k] );
-    pthread_mutex_unlock(&trava);
-    printf("Principal - Numero: %d\tIndice: [%d]\tQntd_Primo %d\n", vetor_numeros[k], k, qntd_primos);
-  }
-  */
+/*Imprimo resultado final*/
 
   printf("%d\n", qntd_primos);
 
@@ -73,10 +62,9 @@ void* funcao_thread(void *arg) {
   int* N = (int*)arg;
   int M = (*N);
 
-  for( int k = M ; k  < qntd_numeros ; k += N_PROCESSOS ){
+  for( int k = M ; k  < qntd_numeros ; k += N_THREADS ){
     pthread_mutex_lock(&trava);
     qntd_primos += verifica_primo( vetor_numeros[k] );
-    printf("Thread %d - Numero: %d\tIndice: [%d]\tQntd_Primo %d\n", M, vetor_numeros[k], k, qntd_primos);
     pthread_mutex_unlock(&trava);
   }
   return 0;
